8ebf75052d8d187973e207792c61d3d5cf9e1552
Dorin <dorin.fodor@gmail.com>
2019-01-05 20:30:31 +0000

    added custom .bashrc with git integration

--- install.sh
+++ install.sh
@@ -16,11 +16,20 @@ do
   [ -f "$DOTFILE" ] && ln -si "$DOTFILE" ~
 done
 
-# Add .my_profile to .profile
+
 echo "Installing custom profile"
-CUSTOM_PROFILE="$HOME/.dotfiles/profile/.my_profile"
+CUSTOM_PROFILE="$HOME/.dotfiles/profile/.profile"
 if ! grep -Fxq "source $CUSTOM_PROFILE" ~/.profile
 then
   echo "source $CUSTOM_PROFILE" >> ~/.profile
   source "$CUSTOM_PROFILE"
+fi
+
+
+echo "Installing custom bashrc"
+CUSTOM_BASHRC="$HOME/.dotfiles/profile/.bashrc"
+if ! grep -Fxq "source $CUSTOM_BASHRC" ~/.bashrc
+then
+  echo "source $CUSTOM_BASHRC" >> ~/.bashrc
+  source "$CUSTOM_BASHRC"
 fi
--- linuxapps.sh
+++ linuxapps.sh
@@ -3,6 +3,6 @@ sudo apt install git
 
 if ! command -v brew > /dev/null 2>&1
 then
-	echo "Installing brew"
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  echo "Installing brew"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 fi
