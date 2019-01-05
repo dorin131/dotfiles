ab282685befccf89d42143132d851f6f6e9faf16
Dorin <dorin.fodor@gmail.com>
2019-01-05 20:03:35 +0000

    install brew and add custom profile

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
-systemType="$(uname -s)"
+SYSTEM_TYPE="$(uname -s)"
 
-if [ "$systemType" = 'Linux' ]; then
-	echo "Running on Linux"
-	echo "Installing apps..."
-	./linuxapps.sh
+if [ "$SYSTEM_TYPE" = 'Linux' ]
+then
+  echo "Running on Linux"
+  echo "Installing apps..."
+  ./linuxapps.sh
 else
-	echo "Running on unknown OS"
+  echo "Running on unknown OS"
 fi
+
 
 echo "Linking dotfiles for Apps..."
 for DOTFILE in `find ~/.dotfiles/apps`
 do
-	[ -f "$DOTFILE" ] && ln -sv "$DOTFILE" ~
+  [ -f "$DOTFILE" ] && ln -si "$DOTFILE" ~
 done
+
+# Add .my_profile to .profile
+echo "Installing custom profile"
+CUSTOM_PROFILE="$HOME/.dotfiles/profile/.my_profile"
+if ! grep -Fxq "source $CUSTOM_PROFILE" ~/.profile
+then
+  echo "source $CUSTOM_PROFILE" >> ~/.profile
+  source "$CUSTOM_PROFILE"
+fi
--- linuxapps.sh
+++ linuxapps.sh
@@ -1,1 +1,8 @@
+echo "Installing git"
 sudo apt install git
+
+if ! command -v brew > /dev/null 2>&1
+then
+	echo "Installing brew"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+fi
